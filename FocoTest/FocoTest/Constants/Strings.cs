0469d1a93432325d7aa11d77d694c9f287fee5be
Ze'ev Goshen <zeevgoshen@gmail.com>
2022-08-06 22:04:11 +0300

    constants file

---
+++ FocoTest/FocoTest/Constants/Strings.cs
@@ -0,0 +1,9 @@
+﻿namespace FocoTest.Constants
+{
+    public static class Strings
+    {
+        public static readonly string NEW_TICKET = "Open";
+        public static readonly string OLD_TICKET = "Closed";
+        public static readonly string SCHEDULED_TEST = "You're test has been scheduled.";
+    }
+}
